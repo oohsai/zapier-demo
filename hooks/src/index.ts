ae0c23bfa0906423574aa683ae10e06fa219e758
oohsai <sadyskiran@gmail.com>
2024-10-04 22:27:27 +0530

    outbox init

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,3 +1,28 @@
 import  express  from "express";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
 
 const app = express();
+
+app.post("/hooks/catch/:userId/:zapId", async (req,res) => {
+    const userId = req.params.userId;
+    const zapId = req.params.zapId;
+    const body = req.body;
+
+    //store in db the new trigger + outbox also
+    await client.$transaction(async tx  => {
+        const run = await client.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: body
+            }
+        });
+
+        await client.zapRunOutBox.create({
+            data: {
+                zapRunId : run.id
+            }
+        })
+    })
+})
