2f1da9a16b9b74c15cf8220fd7224e429a23138d
oohsai <sadyskiran@gmail.com>
2024-10-05 09:26:19 +0530

    added_kafka_worker

---
+++ worker/src/index.ts
@@ -0,0 +1,39 @@
+// import { PrismaClient } from "@prisma/client";
+import { Kafka } from "kafkajs";
+
+// const client = new PrismaClient();
+
+const TOPIC_NAME = "zap-events";
+
+const kafka = new Kafka({
+    clientId:'outbox-processor',
+    brokers:['localhost:9092']
+});
+
+
+async function main() {
+    const consumer = kafka.consumer({groupId:"main-worker"});
+    await consumer.connect();
+    await consumer.subscribe({topic:"zap-events", fromBeginning: true})
+
+    await consumer.run({
+        autoCommit: false,
+        eachMessage: async ({ topic, partition, message }) => {
+        console.log({
+            partition,
+            offset: message.offset,
+            value: message.value?.toString(),
+        })
+        // message is not acknowledged
+        await new Promise(r => setTimeout(r,5000));
+
+        await consumer.commitOffsets([{
+            topic:TOPIC_NAME,
+            partition: partition,
+            offset: (parseInt(message.offset) + 1).toString()
+        }])
+        },
+    })
+}
+
+main()
